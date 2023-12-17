1f295a317060d173c4c24a09d8ae65a9cea8caf8
Dan <danporeda@yahoo.com>
2023-12-17 14:55:22 -0500

    refactored with timer class in separate file

--- app.js
+++ app.js
@@ -1,46 +1,18 @@
-class Timer {
-  constructor(durationInput, startButton, pauseButton) {
-    this.durationInput = durationInput;
-    this.startButton = startButton;
-    this.pauseButton = pauseButton;
-
-    this.startButton.addEventListener('click', this.start);
-    this.pauseButton.addEventListener('click', this.pause);
-    
-  }
-
-  start = () => {
-    this.tick();
-    this.interval = setInterval(this.tick, 200);
-  };
-
-  pause = () => {
-    clearInterval(this.interval);
-  };
-
-  tick = () => {
-    if (this.timeRemaining <= 0) {
-      this.pause();
-    } else {
-      this.timeRemaining = this.timeRemaining - 1;
-    }
-  };
-
-  get timeRemaining() {
-    return parseFloat(this.durationInput.value);
-  }
-
-  set timeRemaining(time) {
-    this.durationInput.value = time;
-  }
-
-}
-
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
 
-const timer = new Timer(durationInput, startButton, pauseButton);
-timer.start();
+const timer = new Timer(durationInput, startButton, pauseButton, {
+  onStart() {
+    console.log('Timer Started');
+  },
+  onTick() {
+    console.log('Timer just dicked down');
+  },
+  onComplete() {
+    console.log('Timer is completed');
+  }
+  });
+
 
 
